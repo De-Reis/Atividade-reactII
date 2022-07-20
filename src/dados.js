fb6c8c873f6cb4c127ba266facca1e4345410f34
Denise Reis <dederh.reis@gmail.com>
2022-07-20 16:58:09 -0300

    commit2

--- src/dados.js
+++ src/dados.js
@@ -13,34 +13,34 @@ const projetos = [
       imgsrc: IMG1,
       link: "https://denise-reis.netlify.app/"
     }
-    ,
+    // ,
 
-    {
-        id: 2,
-        nome: "M de Maravilhosa",
-        descricao: "Um site sobre uma mulher que me inspira, Marielle Franco",
-        aprendizado: "Aprofundando conhecimentos em HTML e CSS",
-        imgsrc: IMG2,
-        link: "https://reprogramaprojeto1-mariellefranco.netlify.app/"
-    },
+    // {
+    //     id: 2,
+    //     nome: "M de Maravilhosa",
+    //     descricao: "Um site sobre uma mulher que me inspira, Marielle Franco",
+    //     aprendizado: "Aprofundando conhecimentos em HTML e CSS",
+    //     imgsrc: IMG2,
+    //     link: "https://reprogramaprojeto1-mariellefranco.netlify.app/"
+    // },
 
-    {
-        id: 3,
-        nome: "Meu diário reprograma",
-        descricao: "Um site em react do meu diário na Reprograma",
-        aprendizado: "Criação de projeto em react, criando componentes e utilizando props",
-        imgsrc: IMG3,
-        link: "https://guileless-liger-fe8995.netlify.app/"
-    },
+    // {
+    //     id: 3,
+    //     nome: "Meu diário reprograma",
+    //     descricao: "Um site em react do meu diário na Reprograma",
+    //     aprendizado: "Criação de projeto em react, criando componentes e utilizando props",
+    //     imgsrc: IMG3,
+    //     link: "https://guileless-liger-fe8995.netlify.app/"
+    // },
 
-    {
-        id: 4,
-        nome: "Meu Portifólio Reprograma",
-        descricao: "Um site em react do meu portifólio na Reprograma",
-        aprendizado: "Criação de projeto em react, criando componentes e utilizando props",
-        imgsrc: IMG4,
-        link: ""
-    },
+    // {
+    //     id: 4,
+    //     nome: "Meu Portifólio Reprograma",
+    //     descricao: "Um site em react do meu portifólio na Reprograma",
+    //     aprendizado: "Criação de projeto em react, criando componentes e utilizando props",
+    //     imgsrc: IMG4,
+    //     link: ""
+    // },
       
     
   ]
